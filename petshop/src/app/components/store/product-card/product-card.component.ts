c31af5c052fcdd0927db45d4ee99925e4ee511c1
Andedy <andrezzaandrade@hotmail.co.uk>
2020-09-01 17:10:58 -0300

    product-card - adicionar produtos no carrinho

--- petshop/src/app/components/store/product-card/product-card.component.ts
+++ petshop/src/app/components/store/product-card/product-card.component.ts
@@ -1,18 +1,31 @@
+import { ToastrService } from 'ngx-toastr';
+import { CartUtil } from './../../../utils/cart.util';
 import { Product } from './../../../models/product.model';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
-  templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  templateUrl: './product-card.component.html'
 })
 export class ProductCardComponent implements OnInit {
+  @Input() product: Product;
 
-  @Input() public product: Product
+  constructor(
+    private toastr: ToastrService
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+  }
 
-  ngOnInit(): void {
+  addToCart() {
+    CartUtil.add(
+      this.product._id,
+      this.product.title,
+      1,
+      this.product.price,
+      this.product.images[0]
+    )
+    this.toastr.success(`${this.product.title} adicionado ao carrinho`, 'Produto Adicionado');
   }
 
 }
