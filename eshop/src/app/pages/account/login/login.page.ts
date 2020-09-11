22aa1478f3b5c322d48794985e9b922567292488
Andedy <andrezzaandrade@hotmail.co.uk>
2020-09-11 17:50:14 -0300

    começando eshop, continuando o petshop (parte do comerciante)

---
+++ eshop/src/app/pages/account/login/login.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoadingController, ToastController, NavController } from '@ionic/angular';
+import { DataService } from 'src/app/data.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  public hide = true
+  public form: FormGroup
+
+  constructor(
+    private fb: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private navCtrl: NavController,
+    private service: DataService,
+  ) {
+    this.form = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.required,
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  toggleHide(){
+    //esse carinha eu quero fazer parecido no painel
+    this.hide = !this.hide;
+  }
+
+}
